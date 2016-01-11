114acfbda6af303b0369e4cf138558e0668acd0d
Noma <postmaster@nomagame.com>
2016-01-11 20:18:35 +0800

    全民坦克游戏设计图
    全民坦克游戏设计图

---
+++ 坦克大战游戏地图/MapTexture/MapRes.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="MapRes" tilewidth="32" tileheight="32">
+ <image source="com.tcgame.Tank_12/assets/ipad/TileMap/MapRes.png" width="160" height="224"/>
+</tileset>
